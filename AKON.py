2242ef733e8fbe2b1602f5a08416a7bf55e88df9
amrdev750 <67783516+amrdev750@users.noreply.github.com>
2025-02-23 00:04:10 +0300

    Add files via upload

---
+++ AKON.py
@@ -0,0 +1,93 @@
+import datetime
+import requests
+import json
+
+
+
+print("""
+**************************************************************
+*                                                            *
+*   █████╗ ██╗  ██╗ ██████╗ ███╗   ██╗    ██████╗ ██╗   ██╗  *
+*  ██╔══██╗██║ ██╔╝██╔═══██╗████╗  ██║    ██╔══██╗╚██╗ ██╔╝  *
+*  ███████║█████╔╝ ██║   ██║██╔██╗ ██║    ██████╔╝ ╚████╔╝   *
+*  ██╔══██║██╔═██╗ ██║   ██║██║╚██╗██║    ██╔══██╗  ╚██╔╝    *
+*  ██║  ██║██║  ██╗╚██████╔╝██║ ╚████║    ██████╔╝   ██║     *
+*  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝    ╚═════╝    ╚═╝     *
+*   █████╗ ███╗   ███╗██████╗                                *
+*  ██╔══██╗████╗ ████║██╔══██╗                               *
+*  ███████║██╔████╔██║██████╔╝                               *
+*  ██╔══██║██║╚██╔╝██║██╔══██╗                               *
+*  ██║  ██║██║ ╚═╝ ██║██║  ██║                               *
+*  ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝                               *
+*                                                            *
+**************************************************************
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Time_Now = datetime.datetime.now
+#Url_Weather_API = "https://api.tomorrow.io/v4/weather/realtime?location=qatar&apikey=ybZNOCqZUFT4JKw7LKgQtrrv0B3c9Lck"
+# Send the GET request
+#response = requests.get(Url_Weather_API)
+# Check if the request was successful (status code 200)
+#if response.status_code == 200:
+    # Parse the response as JSON
+    #data = response.json()
+#temp = data["data"]["values"]["temperature"]
+#print(f"Temperature {temp} cel")
+#precipitationProbability = data["data"]["values"]["precipitationProbability"]
+#print(f"precipitation Probability {precipitationProbability} percent")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(''' 
+
+
+
+
+
+''')
+
+###print(response.text)
+
